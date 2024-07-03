1e90b4e22e43d40b98088baa5ea90f71197ab07d
Diego <diegovaskezm@gmail.com>
2024-07-03 12:49:40 -0400

    main

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="Last-Modified" content="0">
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
     <meta http-equiv="Pragma" content="no-cache">
-    <title>Document</title>
+    <title>Votacion</title>
     <link rel="stylesheet" href="styles.css?<?php echo time(); ?>">
 </head>
 <body class="body-index">
